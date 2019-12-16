1e9213686ec4442e9a215df48eb0e780c7d53c08
Nicholas Marino <nsmarino0@gmail.com>
2019-12-16 15:52:35 -0500

    second commit

--- index.js
+++ index.js
@@ -86,5 +86,5 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`server running on port ${port}`)
+    console.log(`server running on port ${PORT}`)
 })
